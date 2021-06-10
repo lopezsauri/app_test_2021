68dfab59b1b4aabdfed6768515338df2282de19e
Samuel Lopez <lopezsauri.s@gmail.com>
2021-06-10 17:23:00 -0500

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
-  # , only: [:show, :index, :new, :create, :edit, :update] Resourses for article**
-  # You always need CRUD and REST resourses from every object
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
